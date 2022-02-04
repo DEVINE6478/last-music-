996b00bbeb1b074bcc70c27389a98d105d319f8d
DEVINE6478 <98190744+DEVINE6478@users.noreply.github.com>
2022-02-04 16:58:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0;
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+scoreRightWrist=0;
+scoreLeftWrist=0;
+
+function preLoad()
+{
+    song1=loadSound("music.mp3");
+    song2=loadSound("music.mp3");
+}
+
+
+function setup()
+{
+    canvas=createCanvas(500, 500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded()
+{
+    console.log("poseNet is initielized");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist="+scoreRightWrist+"scoreLeftWrist="+scoreLeftWrist);
+
+        rightWristX=results[0].pose.rightWrist.x;        
+        rightWristY=results[0].pose.rightWrist.y;
+
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY);
+
+        leftWristX=results[0].pose.leftWrist.x;        
+        leftWristY=results[0].pose.leftWrist.y;
+
+        console.log("leftWristX="+lefttWristX+"leftWristY="+leftWristY);
+
+
+
+    }
+}
+
+function draw()
+{
+    image(video,0,0,500,500);
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    fill("FF33B2");
+    stroke("FF33B2");
+    if(scoreRightWrist>0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        song2.stop();
+        if(song1_status==false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML="playHarryPotterThemeSong ";
+        }
+    }
+    if(scoreLeftWrist>0.2)
+    {circle(leftWristX, leftWristY, 20);
+    song1.stop();
+    if(song2_status==false)
+    {
+        song2.play();
+        document.getElementById("song").innerHTML="playPeterPan ";
+    }
+    }
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
